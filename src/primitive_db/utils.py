53e8df5bfacd8bb0a63c24fd29e2d61d6bf9333e
RomanchukRoman <r.romanchuk@ya.ru>
2025-10-29 20:55:05 +0300

    create func load_metadata

--- src/primitive_db/utils.py
+++ src/primitive_db/utils.py
@@ -1,11 +1,18 @@
 # src/primitive_db/utils.py
+import json
 
 def load_metadata(filepath):
     '''
     Загружает данные из JSON-файла. Если файл не найден, возвращает пустой словарь {}. 
     Используйте try...except FileNotFoundError.
     '''
-    pass
+    try:
+        with open(filepath) as file:
+            data = json.load(file)
+            print(data) # тут изменить на return
+    except (FileNotFoundError):
+        print({}) # тут изменить на return
+
 
 
 def save_metadata(filepath, data):
@@ -13,3 +20,5 @@ def save_metadata(filepath, data):
     Сохраняет переданные данные в JSON-файл.
     '''
     pass
+
+load_metadata('src/primitive_db/db_meta.json')
