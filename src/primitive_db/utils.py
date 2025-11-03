fae7f53041e5d6040652c2ebd1c3319fe6f7dc88
RomanchukRoman <r.romanchuk@ya.ru>
2025-11-03 17:36:17 +0300

    add func load_table and save_table

--- src/primitive_db/utils.py
+++ src/primitive_db/utils.py
@@ -20,3 +20,22 @@ def save_metadata(data, filepath = METADATA_FILE):
     '''
     with open(filepath, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
+
+def load_table_data(table_name):
+    """
+    Загружает данные таблицы из JSON-файла
+    """
+    filepath = get_table_data_path(table_name)
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+def save_table_data(table_name, data):
+    """
+    Сохраняет данные таблицы в JSON-файл
+    """
+    filepath = get_table_data_path(table_name)
+    with open(filepath, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False, default=str)
